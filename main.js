990e2cfde2ef92c20a7314eb7fe2c84bc0509c87
fscherwi <Florian@ScherwitzlsiMac.home>
2015-09-13 17:03:11 +0200

    add coverage

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 var os = require('os');
 
-var darwin_version = os.release();
-var osx_version;
+function get() {
+  var darwin_version = os.release();
+  var osx_version;
 
-// Darwin <11 and >= 5
-if (11 > darwin_version >= 5) {
-  osx_version = "10." + darwin_version - 4;
-}
-// Darwin >=11
-else {
-  darwin_version = darwin_version.substring(0, darwin_version.length - 1);
-  darwin_version = darwin_version.substring('.', darwin_version.length - 1);
-  osx_version = "10." + darwin_version - 4;
-}
+  if (11 > darwin_version >= 5) {
+    osx_version = "10." + darwin_version - 4;
+  } else {
+    darwin_version = darwin_version.substring(0, darwin_version.length - 1);
+    darwin_version = darwin_version.substring('.', darwin_version.length - 1);
+    osx_version = "10." + (parseFloat(darwin_version) - 4);
+  }
 
-function get_version() {
   return osx_version;
 }
 
-module.exports.get = get_version;
+exports.get = get;
