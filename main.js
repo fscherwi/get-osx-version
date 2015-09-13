7f511bf7309b8d1258890364fa016d864ccccbf0
fscherwi <Florian@ScherwitzlsiMac.home>
2015-09-13 17:34:56 +0200

    minify file

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-var os = require('os');
-
 function get() {
-  var darwin_version = os.release();
-
-  var osx_version = "10." + (parseFloat(darwin_version) - 4);
-
-  return osx_version;
+  return "10." + (parseFloat(require('os').release()) - 4);
 }
-
 exports.get = get;
