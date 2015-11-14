96adef3b3a32a106e637e660b72dbf9f5213a900
fscherwi <Florian@ScherwitzlsiMac.home>
2015-11-14 22:53:40 +0100

    support for nearly all OS X versions

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 exports.get = function get() {
-  return "10." + (parseFloat(require('os').release()) - 4);
+  if (parseFloat(require('os').release()) === 1.3) {
+    return "10.";
+  } else if (require('os').release() === "1.4.1") {
+    return "10.1";
+  } else {
+    return "10." + (parseFloat(require('os').release()) - 4);
+  }
 };
