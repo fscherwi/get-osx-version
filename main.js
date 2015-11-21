49762e752c8af187db2e88c621c5bcfc48982b60
fscherwi <Florian@ScherwitzlsiMac.home>
2015-11-21 14:43:50 +0100

    improvements

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-exports.get = function get() {
+exports.get = function get() { /* istanbul ignore next */
   if (parseFloat(require('os').release()) === 1.3) {
-    return "10.";
-  } else if (require('os').release() === "1.4.1") {
+    return "10.0";
+  } else if (parseFloat(require('os').release()) === 1.4) {
     return "10.1";
   } else {
     return "10." + (parseFloat(require('os').release()) - 4);
--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-console.log(require('./main.js').get() + "\n");
+var osx_version = require('./main.js').get();
+
+console.log('OSX version: ' + osx_version + '\n');
