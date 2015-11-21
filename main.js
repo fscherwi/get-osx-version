999a54d3c61c9c33f7c0e0ca308431e2e34e92fd
fscherwi <Florian@ScherwitzlsiMac.home>
2015-11-21 14:51:28 +0100

    speed improvements

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+var release = parseFloat(require('os').release());
 exports.get = function get() { /* istanbul ignore next */
-  if (parseFloat(require('os').release()) === 1.3) {
+  if (release === 1.3) {
     return "10.0";
-  } else if (parseFloat(require('os').release()) === 1.4) {
+  } else if (release === 1.4) {
     return "10.1";
   } else {
-    return "10." + (parseFloat(require('os').release()) - 4);
+    return "10." + (release - 4);
   }
 };
