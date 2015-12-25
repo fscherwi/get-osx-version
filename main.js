320b518c3809dc4ad481443ab19c9859787a1bfa
fscherwi <Florian@ScherwitzlsiMac.home>
2015-12-25 11:50:54 +0100

    use switch instead of if

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var release = parseFloat(require('os').release());
 exports.get = function get() { /* istanbul ignore next */
-  if (release === 1.3) {
-    return "10.0";
-  } else if (release === 1.4) {
-    return "10.1";
-  } else {
-    return "10." + (release - 4);
+  switch (release) {
+    case 1.3:
+      return "10.0";
+    case 1.4:
+      return "10.1";
+    default:
+      return "10." + (release - 4);
   }
 };
