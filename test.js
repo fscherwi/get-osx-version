aed2cbbf0979eea4745e1694a7f14b5337927fee
fscherwi <Florian@ScherwitzlsiMac.home>
2015-12-31 13:24:10 +0100

    better test script

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-var osx_version = require('./main.js').get();
-if (process.platform === 'darwin') {
-  console.log('\nOSX version: %s\n', osx_version);
-} else {
+var osx_version = require('./main.js').get(); /* istanbul ignore next */
+if (process.platform !== 'darwin') {
   if (osx_version !== 'no OSX system') {
     console.log('ERROR');
     process.exit(1);
-  } else {
-    console.log('\nOSX version: %s\n', osx_version);
   }
 }
+console.log('\nOSX version: %s\n', osx_version);
