c4119cf77c17096874d8f84db6fe304221972195
fscherwi <fscherwi@outlook.com>
2017-09-17 18:35:31 +0200

    improvements

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 exports.get = function get() { /* istanbul ignore next */
-  if (process.platform === 'darwin') {
-    var release = parseFloat(require('os').release());
+  if (process.platform === "darwin") {
+    var release = parseFloat(require("os").release());
     switch (release) {
-      case 1.3:
-        return "10.0";
-      case 1.4:
-        return "10.1";
-      default:
-        return "10." + Number((release - 4).toFixed(1));
+    case 1.3:
+      return "10.0";
+    case 1.4:
+      return "10.1";
+    default:
+      return "10." + (Number((release - 4).toFixed(1)) - 0.1);
     }
   } else {
-    return 'no Mac OS system';
+    return "no Mac OS system";
   }
 };
