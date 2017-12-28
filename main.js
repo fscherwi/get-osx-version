0bad4763f664a5b35a50477253fe536db0c8d271
fscherwi <fscherwi@outlook.com>
2017-12-28 19:57:42 +0100

    improvements

--- main.js
+++ main.js
@@ -7,9 +7,7 @@ exports.get = function get() { /* istanbul ignore next */
     case 1.4:
       return "10.1";
     default:
-      return "10." + (Number((release - 4).toFixed(1)) - 0.1);
+      return "10." + (Number((release - 4.1).toFixed(1)));
     }
-  } else {
-    return "no Mac OS system";
   }
 };
