8f04ebc3181895164860205d92345e7cb27e785b
fscherwi <fscherwi@outlook.com>
2018-08-12 01:36:10 +0200

    various improvements

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-var MacOSVersion = require("./main.js").get(); /* istanbul ignore next */
-if (process.platform !== "darwin") {
-  if (MacOSVersion !== "no Mac OS system") {
-    console.log("ERROR");
+var MacOSVersion = require('./main.js').get(); /* istanbul ignore next */
+if (process.platform !== 'darwin') {
+  if (MacOSVersion !== 'no Mac OS system') {
+    console.log('ERROR');
     process.exit(1);
   }
 }
-console.log("\nMac OS version: %s\n", MacOSVersion);
+console.log('\nMac OS version: %s\n', MacOSVersion);
