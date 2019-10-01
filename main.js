dff7127a0349754fcd9e41b042a325836665490f
fscherwi <fscherwi@users.noreply.github.com>
2019-10-01 22:22:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function get() { /* istanbul ignore next */
 		const release = parseFloat(require('os').release());
 		switch (release) {
 			case 1.3:
-				return 10.0;
+				return '10.0';
 			case 1.4:
-				return 10.1;
+				return '10.1';
 			default:
 				return parseFloat('10.' + (Number((release - 4).toFixed(1))));
 		}
